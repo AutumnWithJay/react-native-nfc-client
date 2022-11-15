02cff6a6c509bf059a7e20c299e7e0d3ef779a3f
Park Joon Woo <devpark.0x1c@gmail.com>
2022-11-15 16:17:05 +0900

    feat: Routing & Set React-Query

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import ErrorPage from './404';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Patrol from './routes/Patrol';
+import PatrolDetail from './routes/PatrolDetail';
+import Employee from './routes/Employee';
+import './style/apt.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'employee',
+        element: <Employee />,
+      },
+      {
+        path: '/patrol',
+        element: <Patrol />,
+      },
+      {
+        path: '/patrol/:id',
+        element: <PatrolDetail />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const queryClient = new QueryClient();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </RecoilRoot>,
+);
